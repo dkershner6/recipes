77b261e2342ca30d2c5fae31e8f0723549393a3b
Derek Kershner <kershner@eformedpartners.com>
2023-02-12 18:26:54 -0800

    Category/Tag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer/themes/github");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -76,6 +76,10 @@ const config = {
                                 label: "All Recipes",
                                 to: "/recipes",
                             },
+                            {
+                                label: "By Category / Tag",
+                                to: "/recipes/tags",
+                            },
                         ],
                     },
                     {
@@ -94,6 +98,10 @@ const config = {
                             {
                                 label: "All Recipes",
                                 to: "/recipes",
+                            },
+                            {
+                                label: "By Category / Tag",
+                                to: "/recipes/tags",
                             },
                         ],
                     },
--- recipes/mac-salad.md
+++ recipes/mac-salad.md
@@ -7,19 +7,20 @@ tags:
     - hawaiian
     - keto
     - bright
+    - fresh
     - tried-and-true
 ---
 
 ## Ingredients
 
--   2-4 lbs of Cauliflower, in 1/2inch x 1/2inch chunks
+-   2 lbs of Cauliflower, in 1/2inch x 1/2inch chunks
 -   1 medium red onion, minced
 -   2 Fresno Peppers, minced
 -   2 cups Castelvetrano Olives, pitted and sliced
--   3 cloves garlic (or more, to taste)
+-   3 cloves garlic, minced (or more, to taste)
 -   1/2 cup Apple Cider Vinegar
 -   1/4 cup Dijon Mustard with whole seeds
--   1 cup Mayonaise
+-   1/2 cup Mayonaise
 -   Salt (to taste)
 -   Pepper (to taste, but use a lot)
 
@@ -28,5 +29,6 @@ tags:
 1. Steam Cauliflower until tender, but still has bite. This is by far the hardest part of the recipe, and when in doubt, pull it out EARLY. Too crunchy is better than too mushy.
 2. In large colander, drain cauliflower, then rinse with copious cold water. This will help it cool down faster.
 3. In large mixing bowl, mix all ingredients together well, starting with the more sauce-like ingredients.
+4. Refreigerate for at least 2 hours, ideally overnight.
 
-Wait a long while, ideally a day or more, then eat. Enjoy!
+Serve at fridge temps, Enjoy!
